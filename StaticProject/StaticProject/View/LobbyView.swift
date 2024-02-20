c331f53a8a763a057e5a97ee6c061430ff32ce4e
kangsworkspace <141600830+kangsworkspace@users.noreply.github.com>
2024-02-20 11:25:10 +0900

    First Commit Healthy

--- StaticProject/StaticProject/View/LobbyView.swift
+++ StaticProject/StaticProject/View/LobbyView.swift
@@ -9,10 +9,26 @@ import SwiftUI
 
 struct LobbyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+
+            VStack {
+                HStack {
+                    Image(nam)
+                }
+                Text("(서울) 화요일 경도 할사람~")
+                Text("양재 양재천 입구")
+                Text("#운동 / 스포츠 | 20대 초반")
+            }
+                    
+            Spacer()
+        }
+        .navigationTitle("메인 화면")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    LobbyView()
+    NavigationStack {
+        LobbyView()
+    }
 }
