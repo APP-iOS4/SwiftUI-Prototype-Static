6b802b8660a8331adc029e369378d9ebb304b082
Hayunho <yunho081544@gmail.com>
2024-02-21 09:35:02 +0900

    회원가입 뷰 추가

--- StaticProject/StaticProject/View/SignupView.swift
+++ StaticProject/StaticProject/View/SignupView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct SignupView: View {
+    @State var id: String = ""
+    @State var password: String = ""
+    @State var passwordConfirm: String = ""
+    @State var loginError: String = "아이디 혹은 비밀번호를 다시 한번 확인해 주세요."
+    @State var isIdEditing: Bool = false
+    @State var isPasswordEditing: Bool = false
+    @State var isPasswordConfirmEditing: Bool = false
+    @State var isLogin: Bool = false
+    
+    let userStore: UserStore = UserStore()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                Text("아이디")
+                TextField("이메일을 입력해주세요.", text: $id,  onEditingChanged:  { edit in
+                    
+                    isIdEditing = edit
+                })
+                    .textFieldStyle(MyTextFieldStyle(focused: $isIdEditing))
+                    .font(.title2)
+
+                
+                Text("비밀번호")
+                SecureField("비밀번호를 입력해주세요", text: $password, onCommit: { isPasswordEditing = false })
+                    .onTapGesture {
+                        isPasswordEditing = true
+                    }
+                    .textFieldStyle(MyTextFieldStyle(focused: $isPasswordEditing))
+                    .font(.title2)
+                
+                Text("비밀번호 확인")
+                SecureField("비밀번호를 입력해주세요", text: $passwordConfirm, onCommit: { isPasswordConfirmEditing = false })
+                    .onTapGesture {
+                        isPasswordConfirmEditing = true
+                    }
+                    .textFieldStyle(MyTextFieldStyle(focused: $isPasswordConfirmEditing))
+                    .font(.title2)
+            }
+            .padding()
+            
+            
+//            Button {
+//                userStore.loginUser(id: id, password: password)
+//            } label: {
+//                Text("로그인")
+//                    .modifier(ButtonTitle())
+//            }
+//            .modifier(ButtonFrame())
+            
+            NavigationLink(destination: LoginView(), label: {
+                Text("회원가입")
+                    .modifier(ButtonTitle())
+            })
+            .modifier(ButtonFrame())
+            
+            Spacer()
+                
+        }
     }
 }
 
