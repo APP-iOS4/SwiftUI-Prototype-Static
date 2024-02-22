27ef121dfe981e039bd50b359a43fb9c118af812
kuksun han <davinci1452@naver.com>
2024-02-22 14:22:21 +0900

    멘트변경

--- StaticProject/StaticProject/View/TabViews/AccountView.swift
+++ StaticProject/StaticProject/View/TabViews/AccountView.swift
@@ -35,17 +35,17 @@ struct AccountView: View {
                 Spacer()
             }
             
-            HStack(spacing:100) {
-                Text("정전기 지수")
+            HStack(spacing:150) {
+                Text("찌리릿 호감도")
                     .font(.title)
                 Text("100W")
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)}
                 Divider()
                 // Spacer()
             
-            Text("정전기 지수가 낮으면 사람들과 연결되기 힘들거에요!\n 낮아지지 않도록 매너있게 행동합시다!")
+            Text("메너가 낮아지면 연결이 살짝 연결이 쉽지 않아요!\n 메너가 올라가면 찌리릿이 신이나서 배불러요.^^\n 찌리릿 호감도가 올라갈수록 찌리릿 굿즈가 기다리고 있어요.")
                 .font(.callout)
-                .frame(width: 400, height: 70)
+                .frame(width: 400, height: 80)
                 .multilineTextAlignment(.center)
                 .background(Color(UIColor.systemGray5))
                 .clipShape(RoundedRectangle(cornerRadius: 10))
@@ -60,29 +60,6 @@ struct AccountView: View {
                 
     }
     
-//            TabView(selection: $selection) {
-//                Text("")
-//                    .tabItem {
-//                        Image(systemName: "house.fill")
-//                        Text("메인화면")
-//                    }.tag(0)
-//                Text("")
-//                    .tabItem {
-//                        Image(systemName: "magnifyingglass")
-//                        Text("검색")
-//                    }.tag(1)
-//                Text("")
-//                    .tabItem {
-//                        Image(systemName: "star.fill")
-//                        Text("참가한방")
-//                    }.tag(2)
-//                Text("")
-//                    .tabItem {
-//                        Image(systemName: "person.crop.circle")
-//                        Text("내 계정")
-//                    }.tag(3)
-//            }
-//                    .font(.headline)
 }
 
 
